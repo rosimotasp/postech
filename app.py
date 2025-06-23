5e3c6697d0381bf35d9571cf63eeab0efb56ac78
rosimotasp <rosim.controladoria@gmail.com>
2025-06-23 08:15:48 -0300

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ st.set_page_config(page_title="Previsão de Obesidade", layout="centered")
 url_modelo = 'https://github.com/rosimotasp/postech/raw/main/modelo_obesidade.pkl'
 from io import BytesIO
 response = requests.get(url_modelo)
+response = requests.get(url_modelo)
 modelo = pickle.load(BytesIO(response.content))
+
 
 
 st.title("🔍 Previsão de Nível de Obesidade")
